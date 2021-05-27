70cb44d888f6149716b98b91f34bdde04ece6b8b
bram2507 <abrahamalvarezgomez2507@gmail.com>
2021-05-27 14:52:47 +0200

    navbar está hecha y responsiva

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,36 @@
 
+var show = true;
+
 function sendEmail(){
 
 }
+
+function open(){
+  
+  
+}
+
+var flag = false;
+function open_menu(){
+ 
+  if(flag){
+    document.getElementById("menu_block").style.display = "none";
+    flag = !flag;
+  }
+  else{
+    document.getElementById("menu_block").style.display = "block";
+    flag = !flag;
+  }
+
+}
+
 function ready(){
   document.getElementById('topButton').addEventListener('click',()=>{
     window.scrollTo(0,0);
   });
-  
 }
+
+
 
 
 
